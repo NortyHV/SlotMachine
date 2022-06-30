ea24cc42864dea0084192a3033a8b8f0d4bfbf9b
NortyHV <nortyhv@gmail.com>
2022-06-30 15:18:20 +0300

    4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import entity.CoinAcceptor;
+
 public class Main {
     public static void main(String[] args) {
+
+        CoinAcceptor coinAcceptor = new CoinAcceptor();
+        coinAcceptor.run();
 
     }
 }
--- src/main/java/entity/CoinAcceptor.java
+++ src/main/java/entity/CoinAcceptor.java
@@ -1,20 +1,65 @@
 package entity;
 
+import java.io.IOException;
+import java.util.Random;
+
 public class CoinAcceptor  {
+    Services services;
+
     int coin;
     int attempts;
+    int toys = 10;
 
+    public CoinAcceptor() {
+        services = new Services();
+    }
 
-    private void payment ( int coin) {
+    public void payment (int coin) {
+        int plays = coin/50;
+        if (plays%4 == 0 && plays>0) plays = plays + plays/4;
+        attempts = plays;
 
     }
 
-    private void winOrNotWin () {
-
-
+    private boolean isWin() {
+        return (Math.random()<=0.2d);
     }
 
+    public void winOrNotWin () {
+        while (attempts-->0) {
+            if (isWin()) {
+                services.write("WIN");
+                if (--toys<2) {
+                    services.write("no toys");
+                }
+            } else {
+                services.write("Lose");
+            }
 
+        }
+    }
+
+    private int readOperation() {
+        String operation;
+        try {
+            if (!(operation = services.read()).equals("exit")) {
+                Integer operationNumber = Integer.parseInt(operation);
+                return operationNumber;
+            }
+        } catch (IOException e) {
+            services.writeUnknownError();
+            readOperation();
+        }
+        return 0;
+    }
+
+    public void run() {
+        int a;
+        while ((a = readOperation()) != 0) {
+            payment(a);
+            winOrNotWin();
+        }
+    }
 
 
 }
